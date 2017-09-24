8231b8de9b9f1922be2455221752f98c031507fc
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2017-09-24 14:25:28 +0100

    Removed unused packages from json. Added bodyparser and favicon to middleware.

--- server/middlewares.js
+++ server/middlewares.js
@@ -3,6 +3,8 @@
 // All middlewares are used here.
 // Check other Koa official middlewares: https://github.com/koajs
 import serve from 'koa-static'
+import favicon from 'koa-favicon'
+import bodyParser from 'koa-bodyparser'
 import config from './config'
 
 export default (app) => {
@@ -12,4 +14,13 @@ export default (app) => {
   // https://github.com/koajs/static
   // https://www.tutorialspoint.com/koajs/koajs_static_files.htm
   app.use(serve(config.static_dir.root))
+
+  //  Add favicon.
+  app.use(favicon(config.static_dir.root + '/favicon.ico'))
+
+  // The parsed body will store in ctx.request.body
+  // If nothing was parsed, body will be an empty object {}
+  // https://github.com/koajs/bodyparser
+  // https://github.com/koajs/koa/issues/719
+  app.use(bodyParser())
 }
